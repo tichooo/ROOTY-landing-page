4523417e7953f777eca7e7945cea311768f78fc9
tichooo <tichooo95@gmail.com>
2025-10-28 22:06:32 +0100

    feat: Add Vercel Web Analytics

--- rooty-landing-page/src/index.js
+++ rooty-landing-page/src/index.js
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./i18n";
+import { Analytics } from '@vercel/analytics/react';
 
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
